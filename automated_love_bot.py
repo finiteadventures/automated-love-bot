da8cdb1e77c20e44bcbd06a95b8a1653b1d0f601
Sarah Quigley <sarah@sarahquigley.net>
2020-02-17 18:00:12 -0500

    WIP: add python script

---
+++ automated_love_bot.py
@@ -0,0 +1,38 @@
+from automated_love_message import AutomatedLoveMessage
+
+class AutomatedLoveBot:
+
+    def send_at_message(self):
+        pass
+
+    def send_direct_message(self):
+        pass
+
+    def schedule_at_messages(self):
+        pass
+
+    def schedule_direct_messages(self):
+        pass
+
+    def schedule_messages(self):
+        pass
+
+class Lover():
+
+    def __init__():
+        pass
+
+    @classmethod
+    def power_lovers(cls):
+        pass
+
+    @classmethod
+    def random
+        pass
+
+    def has_received_at_messsage_in(time)
+        pass
+
+class Messages():
+
+
